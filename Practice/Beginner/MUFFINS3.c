9a3567a2ce522bfda0a67f46ca7c7dc095e86c3a
Amrit Pal Singh <32026156+teamrit@users.noreply.github.com>
2018-02-13 04:10:20 -0500

    Create MUFFINS3.c

---
+++ Practice/Beginner/MUFFINS3.c
@@ -0,0 +1,16 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <math.h>
+    int main()
+    {   int T;
+        scanf("%d",&T);
+        while(T--)
+            {
+                int t,e;
+                scanf("%d",&t);
+                    e=(t/2)+1;
+                printf("%d\n",e);
+            }
+    	return 0;
+    }
+     
